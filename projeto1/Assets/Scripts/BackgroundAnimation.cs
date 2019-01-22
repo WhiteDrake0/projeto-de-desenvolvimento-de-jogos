b585d7223ece5da7651b08cf293dce2eff8ab74b
André Oliveira Gonçalves <dldrake35@gmail.com>
2019-01-22 18:52:56 +0000

    Background completa

--- projeto1/Assets/Scripts/BackgroundAnimation.cs
+++ projeto1/Assets/Scripts/BackgroundAnimation.cs
@@ -6,21 +6,21 @@ public class BackgroundAnimation : MonoBehaviour {
 
 	public Renderer bgRend;
 	public float bgSpeed;
-    //public float tileSizeY;
+    public float tileSizeY;
 
-   // private Vector3 startPosition;
+    private Vector3 startPosition;
 
     void Start()
     {
-       // startPosition = transform.position; 
+        startPosition = transform.position; 
     }
 
 
     // Update is called once per frame
     void Update () {
 
-        bgRend.material.mainTextureOffset += new Vector2(bgSpeed*Time.deltaTime, 0f);	
-        //float newPosition = Mathf.Repeat(Time.time * bgSpeed, tileSizeY);
-        //transform.position = startPosition + Vector3.left * newPosition;
+      //  bgRend.material.mainTextureOffset += new Vector2(bgSpeed*Time.deltaTime, 0f);	
+        float newPosition = Mathf.Repeat(Time.time * bgSpeed, tileSizeY);
+        transform.position = startPosition + Vector3.left * newPosition;
 	}
 }
